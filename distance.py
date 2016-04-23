c6d1fc1418e8871c642c09010635396279607037
Gizem Ece Yılmaz <gizemeceylmaz@gmail.com>
2016-04-23 17:58:51 +0300

    Update distance.py

--- distance.py
+++ distance.py
@@ -19,19 +19,19 @@ print euclideandistance((2,3),(5,4))
 #Purpose: find distance between two points
 #Examples:
 #distance((2,3),(5,4))-> 4
-def manhattendistance(p1,p2):
+def manhattandistance(p1,p2):
 	#Take the sum of the absolute values of the differences of the coordinates.
 	dist=abs(p2[0]-p1[0])+abs(p2[1]-p1[1])
 	return dist
 #test
-print manhattendistance((2,3),(5,4))
+print manhattandistance((2,3),(5,4))
 
 
 #Contract:
 #It returns distance calculation of two points
 #Purpose:to decide which algorithm will be used 
 def choosealgorithm():
-	userinput=raw_input("If you want to use Manhatten Distance type M,otherwise type E for Euclidean Distance: ")
+	userinput=raw_input("If you want to use Manhattan Distance type M,otherwise type E for Euclidean Distance: ")
 	x1=raw_input("Please write first point's x:")
 	y1=raw_input("Please write first point's y:")
 	x2=raw_input("Please write second point's x:")
@@ -39,7 +39,7 @@ def choosealgorithm():
 	point1=(float(x1),float(y1))
 	point2=(float(x2),float(y2))
 	if(userinput=="M"):
-		print manhattendistance(point1,point2)
+		print manhattandistance(point1,point2)
 	elif(userinput=="E"):
 		print euclideandistance(point1,point2)
 
